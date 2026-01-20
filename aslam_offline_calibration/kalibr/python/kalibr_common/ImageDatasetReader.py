a35c6158db970bc5df386aa8353c4079f8c941ba
Filip <filipmymail@gmail.com>
2026-01-20 11:55:29 +0100

    Use images instead of bag files for calibration

--- aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
+++ aslam_offline_calibration/kalibr/python/kalibr_common/ImageDatasetReader.py
@@ -1,14 +1,13 @@
-import cv_bridge
 import cv2
-import rosbag
 import os
+import glob
 import numpy as np
-import pylab as pl
 import aslam_cv as acv
 import sm
 
 
-class BagImageDatasetReaderIterator(object):
+class FolderImageDatasetReaderIterator(object):
+  """Iterator for folder-based image dataset"""
   def __init__(self, dataset, indices=None):
     self.dataset = dataset
     if indices is None:
@@ -30,97 +29,81 @@ class BagImageDatasetReaderIterator(object):
     return self.dataset.getImage(idx)
 
 
-class BagImageDatasetReader(object):
-  def __init__(self, bagfile, imagetopic, bag_from_to=None, perform_synchronization=False, bag_freq=None):
-    self.bagfile = bagfile
-    self.topic = imagetopic
-    self.perform_synchronization = perform_synchronization
-    self.bag = rosbag.Bag(bagfile)
-    self.uncompress = None
-    if imagetopic is None:
-      raise RuntimeError(
-          "Please pass in a topic name referring to the image stream in the bag file\n{0}".format(self.bag))
+class FolderImageDatasetReader(object):
+  """
+  Read images from a folder instead of a ROS bag file.
+  Supports common image formats: jpg, png, bmp, etc.
+  This makes calibration independent of ROS.
+  """
 
-    self.CVB = cv_bridge.CvBridge()
-    # Get the message indices
-    conx = self.bag._get_connections(topics=imagetopic)
-    indices = self.bag._get_indexes(conx)
+  def __init__(self, folder, image_extensions=None, freq=None):
+    """
+    Initialize the folder-based dataset reader.
 
-    try:
-      self.index = next(indices)
-    except:
-      raise RuntimeError("Could not find topic {0} in {1}.".format(imagetopic, self.bagfile))
+    Args:
+      folder: Path to folder containing images
+      image_extensions: List of image extensions to search for (default: ['.jpg', '.png', '.bmp'])
+      freq: Optional frequency to subsample images [Hz]
+    """
+    self.folder = folder
+    self.topic = folder  # Add topic attribute for compatibility
+    self.image_extensions = image_extensions or ['.jpg', '.jpeg', '.png', '.bmp', '.tiff']
+    self.image_files = []
+    self.timestamps = []
 
-    self.indices = np.arange(len(self.index))
+    if not os.path.isdir(folder):
+      raise RuntimeError("Folder does not exist: {0}".format(folder))
 
-    # sort the indices by header.stamp
-    self.indices = self.sortByTime(self.indices)
+    # Find all image files
+    for ext in self.image_extensions:
+      pattern = os.path.join(folder, '*' + ext)
+      self.image_files.extend(sorted(glob.glob(pattern)))
+      pattern = os.path.join(folder, '*' + ext.upper())
+      self.image_files.extend(sorted(glob.glob(pattern)))
 
-    # go through the bag and remove the indices outside the timespan [bag_start_time, bag_end_time]
-    if bag_from_to:
-      self.indices = self.truncateIndicesFromTime(self.indices, bag_from_to)
+    # Remove duplicates while preserving order
+    seen = set()
+    unique_files = []
+    for f in self.image_files:
+      if f not in seen:
+        seen.add(f)
+        unique_files.append(f)
+    self.image_files = unique_files
 
-    # go through and remove indices not at the correct frequency
-    if bag_freq:
-      self.indices = self.truncateIndicesFromFreq(self.indices, bag_freq)
+    if len(self.image_files) == 0:
+      raise RuntimeError("No image files found in folder: {0}".format(folder))
 
-  # sort the ros messegaes by the header time not message time
-  def sortByTime(self, indices):
-    self.timestamp_corrector = sm.DoubleTimestampCorrector()
-    timestamps = list()
-    for idx in self.indices:
-      topic, data, stamp = self.bag._read_message(self.index[idx].position)
-      timestamp = data.header.stamp.secs * 1e9 + data.header.stamp.nsecs
-      timestamps.append(timestamp)
-      if self.perform_synchronization:
-        self.timestamp_corrector.correctTimestamp(data.header.stamp.to_sec(),
-                                                  stamp.to_sec())
+    # Generate timestamps based on file modification time or sequential numbering
+    for idx, filepath in enumerate(self.image_files):
+      try:
+        # Try to use file modification time
+        mtime = os.path.getmtime(filepath)
+        timestamp = mtime
+      except:
+        # Fallback to sequential timestamps with 0.1s spacing
+        timestamp = idx * 0.1
+      self.timestamps.append(timestamp)
 
-    sorted_tuples = sorted(zip(timestamps, indices))
-    sorted_indices = [tuple_value[1] for tuple_value in sorted_tuples]
-    return sorted_indices
+    # Create indices array
+    self.indices = np.arange(len(self.image_files))
 
-  def truncateIndicesFromTime(self, indices, bag_from_to):
-    # get the timestamps
-    timestamps = list()
-    for idx in self.indices:
-      topic, data, stamp = self.bag._read_message(self.index[idx].position)
-      timestamp = data.header.stamp.secs + data.header.stamp.nsecs / 1.0e9
-      timestamps.append(timestamp)
+    # Apply frequency filtering if specified
+    if freq:
+      self.indices = self.truncateIndicesFromFreq(self.indices, freq)
 
-    bagstart = min(timestamps)
-    baglength = max(timestamps) - bagstart
-
-    # some value checking
-    if bag_from_to[0] >= bag_from_to[1]:
-      raise RuntimeError("Bag start time must be bigger than end time.".format(bag_from_to[0]))
-    if bag_from_to[0] < 0.0:
-      sm.logWarn("Bag start time of {0} s is smaller 0".format(bag_from_to[0]))
-    if bag_from_to[1] > baglength:
-      sm.logWarn("Bag end time of {0} s is bigger than the total length of {1} s".format(
-          bag_from_to[1], baglength))
-
-    # find the valid timestamps
-    valid_indices = []
-    for idx, timestamp in enumerate(timestamps):
-      if timestamp >= (bagstart + bag_from_to[0]) and timestamp <= (bagstart + bag_from_to[1]):
-        valid_indices.append(idx)
-    sm.logWarn(
-        "BagImageDatasetReader: truncated {0} / {1} images (from-to).".format(len(indices) - len(valid_indices), len(indices)))
-    return valid_indices
+    sm.logInfo("FolderImageDatasetReader: Found {0} images in folder: {1}".format(
+        len(self.indices), folder))
 
   def truncateIndicesFromFreq(self, indices, freq):
-
-    # some value checking
+    """Subsample images to match desired frequency"""
     if freq < 0.0:
       raise RuntimeError("Frequency {0} Hz is smaller 0".format(freq))
 
     # find the valid timestamps
     timestamp_last = -1
     valid_indices = []
-    for idx in self.indices:
-      topic, data, stamp = self.bag._read_message(self.index[idx].position)
-      timestamp = data.header.stamp.secs + data.header.stamp.nsecs / 1.0e9
+    for idx in indices:
+      timestamp = self.timestamps[idx]
       if timestamp_last < 0.0:
         timestamp_last = timestamp
         valid_indices.append(idx)
@@ -128,81 +111,53 @@ class BagImageDatasetReader(object):
       if (timestamp - timestamp_last) >= 1.0 / freq:
         timestamp_last = timestamp
         valid_indices.append(idx)
+
     sm.logWarn(
-      "BagImageDatasetReader: truncated {0} / {1} images (frequency)".format(len(indices) - len(valid_indices), len(indices)))
+      "FolderImageDatasetReader: truncated {0} / {1} images (frequency)".format(
+          len(indices) - len(valid_indices), len(indices)))
     return valid_indices
 
   def __iter__(self):
-    # Reset the bag reading
+    """Reset and return iterator"""
     return self.readDataset()
 
   def readDataset(self):
-    return BagImageDatasetReaderIterator(self, self.indices)
+    """Return an iterator over the dataset"""
+    return FolderImageDatasetReaderIterator(self, self.indices)
 
   def readDatasetShuffle(self):
-    indices = self.indices
+    """Return an iterator over the shuffled dataset"""
+    indices = self.indices.copy()
     np.random.shuffle(indices)
-    return BagImageDatasetReaderIterator(self, indices)
+    return FolderImageDatasetReaderIterator(self, indices)
 
   def numImages(self):
+    """Return the number of images in the dataset"""
     return len(self.indices)
 
   def getImage(self, idx):
-    topic, data, stamp = self.bag._read_message(self.index[idx].position)
-    if self.perform_synchronization:
-      timestamp = acv.Time(self.timestamp_corrector.getLocalTime(
-          data.header.stamp.to_sec()))
-    else:
-      timestamp = acv.Time(data.header.stamp.secs,
-                           data.header.stamp.nsecs)
-    if data._type == 'mv_cameras/ImageSnappyMsg':
-      if self.uncompress is None:
-        from snappy import uncompress
-        self.uncompress = uncompress
-      img_data = np.reshape(self.uncompress(np.fromstring(
-          data.data, dtype='uint8')), (data.height, data.width), order="C")
-    elif data._type == 'sensor_msgs/CompressedImage':
-      # compressed images only have either mono or BGR normally (png and jpeg)
-      # https://github.com/ros-perception/vision_opencv/blob/906d326c146bd1c6fbccc4cd1268253890ac6e1c/cv_bridge/src/cv_bridge.cpp#L480-L506
-      img_data = np.array(self.CVB.compressed_imgmsg_to_cv2(data))
-      if len(img_data.shape) > 2 and img_data.shape[2] == 3:
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
-    elif data._type == 'sensor_msgs/Image':
-      if data.encoding == "16UC1" or data.encoding == "mono16":
-        image_16u = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = (image_16u / 256).astype("uint8")
-      elif data.encoding == "8UC1" or data.encoding == "mono8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-      elif data.encoding == "8UC3" or data.encoding == "bgr8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BGR2GRAY)
-      elif data.encoding == "rgb8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_RGB2GRAY)
-      elif data.encoding == "8UC4" or data.encoding == "bgra8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BGRA2GRAY)
-      # bayes encodings conversions from
-      # https://github.com/ros-perception/image_pipeline/blob/6caf51bd4484ae846cd8a199f7a6a4b060c6373a/image_proc/src/libimage_proc/processor.cpp#L70
-      elif data.encoding == "bayer_rggb8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BAYER_BG2GRAY)
-      elif data.encoding == "bayer_bggr8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BAYER_RG2GRAY)
-      elif data.encoding == "bayer_gbrg8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BAYER_GR2GRAY)
-      elif data.encoding == "bayer_grbg8":
-        img_data = np.array(self.CVB.imgmsg_to_cv2(data))
-        img_data = cv2.cvtColor(img_data, cv2.COLOR_BAYER_GB2GRAY)
-      else:
-        raise RuntimeError(
-            "Unsupported Image Encoding: '{}'\nSupported are: "
-            "16UC1 / mono16, 8UC1 / mono8, 8UC3 / rgb8 / bgr8, 8UC4 / bgra8, "
-            "bayer_rggb8, bayer_bggr8, bayer_gbrg8, bayer_grbg8".format(data.encoding))
-    else:
-      raise RuntimeError(
-        "Unsupported Image Type: '{}'\nSupported are: "
-        "mv_cameras/ImageSnappyMsg, sensor_msgs/CompressedImage, sensor_msgs/Image".format(data._type))
+    """
+    Load and return an image with its timestamp.
+
+    Args:
+      idx: Index in the indices array
+
+    Returns:
+      Tuple of (timestamp, image_array)
+    """
+    actual_idx = self.indices[idx]
+    filepath = self.image_files[actual_idx]
+    timestamp_sec = self.timestamps[actual_idx]
+
+    # Convert timestamp to aslam::Time format (seconds and nanoseconds)
+    secs = int(timestamp_sec)
+    nsecs = int((timestamp_sec - secs) * 1e9)
+    timestamp = acv.Time(secs, nsecs)
+
+    # Load image using OpenCV
+    img_data = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+
+    if img_data is None:
+      raise RuntimeError("Could not read image file: {0}".format(filepath))
+
     return (timestamp, img_data)
